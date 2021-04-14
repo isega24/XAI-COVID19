3c244e927ac8c72208f7981148d11ab863cbb993
isega24 <isevillano@hotmail.com>
2021-04-14 17:21:13 +0000

    Scripts of first experiments

---
+++ COVID-XAI/load_data.py
@@ -0,0 +1,66 @@
+import os
+import torch
+from skimage import io
+import sys
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import numpy as np
+import torchvision
+
+
+class COVIDGR(Dataset):
+    def __init__(self,data_folder="./data/COVIDGR-09-04/Revised/",
+            metadata_file="./data/COVIDGR-09-04/Metadatos.csv",
+            transform = None):
+        self.data_folder = data_folder
+        self.metadata_file = metadata_file
+        self.dataset = []
+        self.transform = transform
+
+        if self.transform is None:
+            self.transform = self._default_transform
+        with open(metadata_file,"r") as csv_file:
+            for i, line in enumerate(csv_file):
+                line = line.split(",")
+                if i > 0:
+                    filename = line[2]
+                    if not os.path.exists(f"{data_folder}{filename}.jpg"):
+                        print(f"Row: {i} ID: {line[0]} Date: {line[1]} File: {data_folder}{filename}.jpg")
+
+                    elif line[7] == "PA":
+                        clase = line[6]
+                        if clase in ["VP","FN"]:
+                            clase = [1,0]
+                        elif clase in ["VN","FP"]:
+                            clase = [0,1]
+                        self.dataset.append((f"{filename}.jpg",clase))
+    def __len__(self):
+        return len(self.dataset)
+    def __getitem__(self,idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        img_name = os.path.join(self.data_folder, self.dataset[idx][0])
+        image = io.imread(img_name,as_gray=True)
+
+        clase = np.array([self.dataset[idx][1]])
+        sample = image,clase
+
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+
+    def _default_transform(self,sample):
+        sample_ = [None,None]
+        sample_[0] = torch.unsqueeze(torch.Tensor(sample[0]),dim=0)*255
+        sample_[1] =  torch.Tensor(sample[1])[0]
+        return sample_
+    
+    def addTransform(self,new_transform):
+        self.transform = torchvision.transforms.Compose([self.transform,
+        new_transform])
+
+if __name__ == "__main__":
+    data = COVIDGR(data_folder="./data/COVIDGR-09-04/Revised-croped/")
+    
+    for d in data:
+        print(d[0].shape,d[1].shape)
